06d5cf24ea094bb4525d64e6d70054bad4894285
juliomassignan <juliomassignan@users.noreply.github.com>
2021-05-11 16:12:09 -0300

    Add files via upload

---
+++ Question#2/Readme.md
@@ -0,0 +1,7 @@
+# SiemensQuestionary_JulioMassignan
+This repository contains the solution of the candidate Julio Massignan in the Programming skills and PL/SQL in the Siemens Job Application.
+
+
+1. Each subfolder has a README file with comments, and aditional files with the codes and 
+2. The README file also provides a link to the OnLineGDB and OnLineSQL tests
+
---
+++ Question#2/main.cpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+
+Techincal Assessment - Siemens Job Interview
+Author: Julio Massignan
+Date: 12th May 2021
+
+
+Code and solution for question #2
+
+Requirements:
+    For each multiple of 3, print "Foo" instead of the
+    number.
+    For each multiple of 5, print "Baa" instead of the
+    number.
+    For multiple numbers simultaneously of 3 and 5,
+    print "FooBaa", instead of the number.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+
+/* Function to print a list of numbers according to the questionary requirement
+Inputs:
+    int init: begin of the integer list
+    int end: end of the integer list
+
+Outputs:
+    none (prints on the screen)
+
+*/
+void printLine_Q2(int init, int end){
+    //---------------------------------------------
+    // For loop to iterate over the list of numbers
+    for (int i = init; i <= end; i++ ){
+        // Test if multiple of 3 and 5 simultaneously
+        if (((i % 3) == 0) && ((i % 5) == 0)){
+            cout << "FooBaa" << "\n";		
+        }
+        // Test if multiple of 3 only
+        else if ((i % 3) == 0) {
+            cout << "Foo" << "\n";		
+        }
+        // Test if multiple of 5 only
+        else if ((i % 5) == 0) {
+            cout << "Baa" << "\n";		
+        }
+        // Otherwise print the number
+        else{
+            cout << i << "\n";		
+        }
+    }
+}
+
+// Main source code with callable function
+int main()
+{
+    // Call function that print list of number according to the requirements, from 1 to 100
+    printLine_Q2(1, 100);
+
+    return 0;
+}
