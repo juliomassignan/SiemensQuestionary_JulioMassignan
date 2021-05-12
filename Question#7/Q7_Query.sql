a5867261f6c30b4c9814d9c6b2c5c534e84dc9d3
juliomassignan <juliomassignan@users.noreply.github.com>
2021-05-11 23:43:15 -0300

    Delete Question#7 directory

--- Question#7/Q7_Query.sql
+++
@@ -1,5 +0,0 @@
--- Select names from both dogs and cats in the same table -  UNION already queries distinct values
-SELECT Name FROM dogs UNION SELECT Name FROM cats
-
-
--- Repeated names: Mel, Monica, Branco
